cace3056da49463121c238295d1445087bdbe0a3
Akshaya Venky <akshaya-venkatesh8@users.noreply.github.com>
2016-06-06 15:27:21 +0530

    Create Address.java

---
+++ Address.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.akshayavenkatesh8;
+
+/**
+ *
+ * @author akshaya
+ */
+class Address {
+        public String name;
+        public String streetNumber;
+        public String streetName;
+        public String secondaryAddress;
+        public String city;
+        public String stateCode;
+        public String zipCode;
+        
+        public void setName(String name)
+        {
+            this.name=name;
+        }
+        
+        public void setStreetNumber(String streetNumber)
+        {
+            this.streetNumber=streetNumber;
+        }
+        public void setStreetName(String streetName)
+        {
+            this.streetName=streetName;
+        }
+        public void setSecondaryAddress(String secondaryAddress)
+        {
+            this.secondaryAddress=secondaryAddress;
+        }
+        public void setCity(String city)
+        {
+            this.city=city;
+        }
+        public void setStateCode (String stateCode)
+        {
+            this.stateCode=stateCode;
+        }
+        public void setZipCode (String zipCode)
+        {
+            this.zipCode=zipCode;
+        }
+        public String toString()
+        {
+           return "Address :\n"+name+",\n"+streetNumber+" "+streetName+",\n"+secondaryAddress+",\n"+city+",\n"+stateCode+" "+zipCode;
+        }
+}
