f9967e72e245fd8c572fc830b21d8e3ac667e1c6
Akshaya Venky <akshaya-venkatesh8@users.noreply.github.com>
2016-06-06 15:24:20 +0530

    Create SAXParsing.java

---
+++ SAXParsing.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.akshayavenkatesh8;
+
+import java.io.File;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import org.xml.sax.SAXException;
+
+/**
+ *
+ * @author akshaya
+ */
+public class SAXParsing {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws ParserConfigurationException, SAXException {
+        // TODO code application logic here
+    
+      try {	
+        File inputFile = new File("address.xml");
+         SAXParserFactory factory = SAXParserFactory.newInstance();
+         SAXParser saxParser = factory.newSAXParser();
+         UserHandler userhandler = new UserHandler();
+         saxParser.parse(inputFile, userhandler);
+         Address add= new Address();
+         add= userhandler.getAddress();
+         System.out.println(add);      
+          } 
+         catch (Exception e) {
+             System.out.println(e);
+      }
+    }
+  }
