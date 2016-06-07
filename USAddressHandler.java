aaea054d0547beec0e68c98ecaafd7ad63b37c7a
Akshaya Venky <akshaya-venkatesh8@users.noreply.github.com>
2016-06-07 10:44:28 +0530

    Update and rename UserHandler.java to USAddressHandler.java

--- USAddressHandler.java
+++ USAddressHandler.java
@@ -11,38 +11,38 @@ import org.xml.sax.helpers.DefaultHandler;
  *
  * @author akshaya
  */
-class UserHandler extends DefaultHandler {
+class USAddressHandler extends DefaultHandler {
     public Address address = new Address();
     public String temp;
     @Override
    public void startElement(String uri, String localName, String qName, Attributes att) throws SAXException {
        
-       temp="";
+       elementContent = "";
        
    }
     @Override
    public void endElement(String uri, String localName, String qName)  {
              if (qName.equalsIgnoreCase("name")) {
-             address.setName(temp);
+             address.setName(elementContent);
               } else if (qName.equalsIgnoreCase("streetnumber")) {
-                     address.setStreetNumber(temp);
+                     address.setStreetNumber(elementContent);
               } else if (qName.equalsIgnoreCase("streetname")) {
-                     address.setStreetName(temp);
+                     address.setStreetName(elementContent);
               } else if (qName.equalsIgnoreCase("secondaryaddress")) {
-                     address.setSecondaryAddress(temp);
+                     address.setSecondaryAddress(elementContent);
               } else if (qName.equalsIgnoreCase("city")) {
-                     address.setCity(temp);
+                     address.setCity(elementContent);
               } else if (qName.equalsIgnoreCase("statecode")) {
-                     address.setStateCode(temp);
+                     address.setStateCode(elementContent);
               } else if (qName.equalsIgnoreCase("zipcode")) {
-                     address.setZipCode(temp);
+                     address.setZipCode(elementContent);
               }
 }
 
       @Override
    public void characters(char[] buffer, int start, int length)  {
       
-         temp = new String(buffer, start, length);
+         elementContent = new String(buffer, start, length);
         
    }
    public Address getAddress()
