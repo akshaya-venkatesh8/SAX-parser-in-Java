d53369fc4609a92463e05a96d8f83187ccc1d7a3
Akshaya Venky <akshaya-venkatesh8@users.noreply.github.com>
2016-06-07 14:52:07 +0530

    Update InvalidXMLErrorHandler.java

--- InvalidXMLErrorHandler.java
+++ InvalidXMLErrorHandler.java
@@ -10,20 +10,23 @@ import org.xml.sax.SAXParseException;
  * @author akshaya
  */
 class InvalidXMLErrorHandler implements ErrorHandler{
-
+    String check;
     @Override
     public void warning(SAXParseException exception) throws SAXException {
         System.out.println("ERROR : "+exception.getMessage());
+         check = "Invalid";
     }
 
     @Override
     public void error(SAXParseException exception) throws SAXException {
         System.out.println("ERROR : "+exception.getMessage());
+         check = "Invalid";
     }
 
     @Override
     public void fatalError(SAXParseException exception) throws SAXException {
         System.out.println("ERROR : "+exception.getMessage());
+         check = "Invalid";
     }
     
 }
